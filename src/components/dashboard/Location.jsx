ea3eb2f96303abccbe4278119f338102f04387e2
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2023-02-01 22:08:22 +0000

    Basic location dashboard

--- src/components/dashboard/Location.jsx
+++ src/components/dashboard/Location.jsx
@@ -5,6 +5,7 @@ export default function Location() {
     const params = useParams()
 
     const [location, setLocation] = useState({})
+    const [members, setMembers] = useState({})
     const [counter, count] = useState(0)
 
     if (counter == 0) {
@@ -13,14 +14,20 @@ export default function Location() {
             setLocation(payload)
             count(1)
         })
-    } 
-    if (!location[0]) return <p>Loading location...</p>
+    } else if (counter == 1) {
+        console.log("hmmm") 
+        fetch("http://localhost:8080/api/"+params.id+"/memberships").then(data => data.json()).then(payload => {
+            setMembers(payload)
+            count(2)
+        })
+    }
+    if (!location[0] || !members) return <p>Loading location...</p>
     return (
         <div>
-            <h1>{location[0].name}</h1>
-            <h3>Owner: {location[0].user}</h3>
-            <h3>ID: {location[0]._id}</h3>
-
+            <h1 className="loc-head">{location[0].name}</h1>
+            <h3 className="loc-2">Owner: {location[0].user}</h3>
+            <h3 className="loc-2">ID: {location[0]._id}</h3>
+            <h3 className="loc-2">Memberships: {members.length}</h3>
         </div>
     )
 }
